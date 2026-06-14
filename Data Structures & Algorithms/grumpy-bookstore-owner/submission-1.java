fe246031d3803bfe6824c603b7cd43b429221f13
Devanand <anand.dev.1985@gmail.com>
2026-06-14 08:31:35 +0200

    Add: grumpy-bookstore-owner - submission-1

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int satisfied = 0;
+        for (int i = 0; i < customers.length; i++) {
+            if (grumpy[i] == 0) {
+                satisfied += customers[i];
+            }
+        }
+
+        int gain = 0;
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i] == 1) {
+                gain += customers[i];
+            }
+        }
+
+        int maxGain = gain;
+        for (int i = minutes; i < customers.length; i++) {
+            if (grumpy[i] == 1) {
+                gain += customers[i];
+            }
+            if (grumpy[i - minutes] == 1) {
+                gain -= customers[i - minutes];
+            }
+            if (gain > maxGain) {
+                maxGain = gain;
+            }
+        }
+        
+        return satisfied + maxGain;
+    }
+}
