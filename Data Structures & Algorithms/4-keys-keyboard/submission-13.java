3e9a3edf7a79d261501dd91be8f00ee7bb50a35d
Devanand <anand.dev.1985@gmail.com>
2026-06-15 20:28:01 +0200

    Add: 4-keys-keyboard - submission-13

---
+++ Data Structures & Algorithms/4-keys-keyboard/submission-13.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxA(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 1; j <= i - 3; j++) {
+                dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+            }
+        }
+        return dp[n];
+    }
+}
