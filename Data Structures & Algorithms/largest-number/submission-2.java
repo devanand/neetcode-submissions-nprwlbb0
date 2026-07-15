c9e0c22471c1b216522df49e3555993fe8a739e5
Devanand <anand.dev.1985@gmail.com>
2026-07-16 00:03:44 +0200

    Add: largest-number - submission-2

---
+++ Data Structures & Algorithms/largest-number/submission-2.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String[] arr = Arrays.stream(nums).mapToObj(String::valueOf)
+        .toArray(String[]::new);
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a+b));
+        String res = String.join("", arr);
+        return res.startsWith("0") ? "0" : res;
+    }
+}
